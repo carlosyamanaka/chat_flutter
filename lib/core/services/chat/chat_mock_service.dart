f3ca8c6d0796f929a42b47e1bd9059e167215e96
iTrue <109562378+carlosyamanaka@users.noreply.github.com>
2024-09-23 10:05:05 -0300

    Criado o ChatService com o controller utilizando Stream dados

---
+++ lib/core/services/chat/chat_mock_service.dart
@@ -0,0 +1,60 @@
+import 'dart:async';
+import 'dart:math';
+
+import 'package:chat_flutter/core/models/chat_message.dart';
+import 'package:chat_flutter/core/models/chat_user.dart';
+import 'package:chat_flutter/core/services/chat/chat_service.dart';
+
+class ChatMockService implements ChatService {
+  static final List<ChatMessage> _msgs = [
+    ChatMessage(
+      id: '1',
+      text: 'Bom dia',
+      createdAt: DateTime.now(),
+      userId: '123',
+      userName: 'Joao',
+      userImageUrl: 'assets/images/avatar.png',
+    ),
+    ChatMessage(
+      id: '2',
+      text: 'Bom dia. Teremos reunião hoje?',
+      createdAt: DateTime.now(),
+      userId: '456',
+      userName: 'Pedro',
+      userImageUrl: 'assets/images/avatar.png',
+    ),
+    ChatMessage(
+      id: '3',
+      text: 'Sim, pode ser agora!',
+      createdAt: DateTime.now(),
+      userId: '123',
+      userName: 'João',
+      userImageUrl: 'assets/images/avatar.png',
+    ),
+  ];
+  static MultiStreamController<List<ChatMessage>>? _controller;
+  static final _msgsStream = Stream<List<ChatMessage>>.multi((controller) {
+    _controller = controller;
+    controller.add(_msgs);
+  });
+
+  @override
+  Stream<List<ChatMessage>> messagesStream() {
+    return _msgsStream;
+  }
+
+  @override
+  Future<ChatMessage> save(String text, ChatUser user) async {
+    final newMessage = ChatMessage(
+      id: Random().nextDouble().toString(),
+      text: text,
+      createdAt: DateTime.now(),
+      userId: user.id,
+      userName: user.name,
+      userImageUrl: user.imageUrl,
+    );
+    _msgs.add(newMessage);
+    _controller?.add(_msgs);
+    return newMessage;
+  }
+}
--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,3 +1,5 @@
+import 'package:chat_flutter/components/messages.dart';
+import 'package:chat_flutter/components/new_messages.dart';
 import 'package:chat_flutter/core/services/auth/auth_service.dart';
 import 'package:flutter/material.dart';
 
@@ -7,17 +9,45 @@ class ChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('Chat page'),
-            TextButton(
-              onPressed: () {
-                AuthService().logout();
-              },
-              child: const Text('Logout'),
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        foregroundColor: Colors.white,
+        centerTitle: true,
+        title: const Text('Chat Flutter'),
+        actions: [
+          DropdownButton(
+            icon: const Icon(
+              Icons.more_vert,
+              color: Colors.white,
             ),
+            items: const [
+              DropdownMenuItem(
+                value: 'logout',
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.exit_to_app,
+                      color: Colors.black87,
+                    ),
+                    SizedBox(width: 10),
+                    Text('Sair'),
+                  ],
+                ),
+              ),
+            ],
+            onChanged: (value) {
+              if (value == 'logout') {
+                AuthService().logout();
+              }
+            },
+          ),
+        ],
+      ),
+      body: const SafeArea(
+        child: Column(
+          children: [
+            Expanded(child: Messages()),
+            NewMessages(),
           ],
         ),
       ),
